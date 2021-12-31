7b27bec6431b816a4753c674a5bee44725362d00
M7MDSG76 <mohammed.s.alghanmi@gmail.com>
2021-12-31 20:59:11 +0300

    Add project files.

---
+++ division/Division.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace division
+{
+    class Division
+    {
+        public int division(int x, int y)
+        {
+            
+            int Division = x / y;
+            return Division;
+        }
+        public void App()
+        {
+            try
+            {
+                Console.WriteLine("Enter first number: ");
+                int FirstNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter secound number: ");
+                int SecoundNum = int.Parse(Console.ReadLine());
+                
+                Console.WriteLine(division(FirstNum, SecoundNum));
+            }
+            catch (FormatException e) {
+                Console.WriteLine("inviled number. Please try again.");
+            }catch(DivideByZeroException e)
+            {
+                Console.WriteLine("cannot divided by zero. Pleas try again.");
+            }
+        }
+
+
+    }
+}
---
+++ division/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Division x = new Division();
+            x.App();
+        }
+    }
+}
